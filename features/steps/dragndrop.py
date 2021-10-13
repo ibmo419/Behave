7ad01f35c3ecc15f895a64601c46e19c6f435947
Mohamed Ali <77637821+ibmo419@users.noreply.github.com>
2021-10-13 19:12:43 +0100

    first commit

---
+++ features/steps/dragndrop.py
@@ -0,0 +1,24 @@
+from behave import given, when, then
+from numpy.testing import assert_equal
+from selenium import webdriver
+import time
+
+from selenium.webdriver.common.action_chains import ActionChains
+
+@given(u'the user navigate to drag-drop home page')
+def step_impl(context):
+    context.dd.setup()
+    # context.browser.get('https://qavbox.github.io/demo/dragndrop/')
+
+
+
+@when(u'he drag the rectangle in his taget (green rectangle)')
+def step_impl(context):
+    context.dd.drag()
+    time.sleep(2)
+
+
+@then(u'"Droped!" message will be displayed in the target plce')
+def step_impl(context):
+    assert_equal(context.dd.result(), "Dropped")
+
---
+++ features/steps/slider_bar.py
@@ -0,0 +1,16 @@
+from behave import when, then
+from numpy.testing import assert_equal
+import time
+
+from selenium.webdriver import ActionChains
+
+
+@when(u'he drag and drop the slider bar')
+def step_impl(context):
+    context.dd.slider()
+    time.sleep(2)
+
+
+@then(u'the value 100 will be displayed')
+def step_impl(context):
+    assert_equal(context.dd.verif_slider(), "100")
